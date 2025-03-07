34d7c450d876dccbb6e029fc42144974a15d16de
Timothy-Ho0203 <156638648+Timothy-Ho0203@users.noreply.github.com>
2025-03-07 23:50:52 +0800

    feat: Add about section

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
         className="text-center z-10 px-4"
       >
         <h1 
-          className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 
+          className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 
             bg-clip-text text-transparent leading-right"
         >
           Hi, I'm Timothy.
